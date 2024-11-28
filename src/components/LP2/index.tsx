832aa91cf71c33efab92d60fcdc290e73d39a0e5
coffeecupjapan <coffeecupjapan@yahoo.co.jp>
2024-11-28 21:46:43 +0900

    impl lp2

--- src/components/LP2/index.tsx
+++ src/components/LP2/index.tsx
@@ -75,12 +75,20 @@ const section1Title = {
     lineHeight: "0px",
     margin: "-20px auto"
 }
+const section1WhyExplain = {
+    fontSize: "30px",
+    lineHeight: "64px",
+    marginBottom: "0px"
+}
+const section1Hr = {
+    width: "200px"
+}
 const section1Explain = {
     fontSize: "16px",
-    lineHeight: "46px"
+    lineHeight: "26px"
 }
 const section1ExplainSmall = {
-    color: "#AAAAAA"
+    color: "#AAAAAA",
 }
 // section 2
 const section2Wrapper = {
@@ -153,6 +161,8 @@ const LP2 = () => {
                         Amazonギフト<span style={section03000yen}>3000円</span>獲得！
                     </h3>
                     <p style={{...section0LeftContent}}>
+                        <ModeEditOutlineIcon/> <strong><a href="#why" style={{color:"black"}}>Why?</a></strong>
+                        <br/>
                         <ModeEditOutlineIcon/> <strong><a href="#overview" style={{color:"black"}}>概要{"　"}Overview</a></strong>
                         <br/>
                         <ModeEditOutlineIcon/> <strong><a href="#participate" style={{color:"black"}}>参加方法{"　"}How to participate</a></strong>
@@ -163,14 +173,36 @@ const LP2 = () => {
             <hr style={section0Hr2}/>
             <Box sx={{...section1, paddingBottom: "0px"}}>
                 <Box sx={section1Title}>
+                    <h3 id="why">Why？</h3>
+                </Box>
+                <h5 style={section1WhyExplain}>
+                    あなたの<span style={section03000yen}>履歴書・職務経歴書</span>は技術力をアピールできてますか？
+                    <br/>
+                    文字だけでは、本当の技術力が伝わるか分からなくて心配・・・
+                    <br/>
+                    その心配を解消するために、<span style={section03000yen}>技術力の証拠</span>をアピールしませんか？
+                    <hr style={section1Hr}/>
+                    この検索エンジンは、あなたの検索履歴から技術記事を自動生成します
+                    <br/>
+                    企業様にアピールしたい技術の記事を<span style={section03000yen}>自動生成</span>して、
+                    <br/>
+                    <span style={section03000yen}>技術的な根拠のある職務経歴書</span>を作りましょう！
+                </h5>
+                <br/><br/>
+                <Box sx={section1Title}>
                     <h3 id="overview">概要{"　"}Overview</h3>
                 </Box>
                 <br/>
+                <h5 style={section1WhyExplain}>
+                    エンジニアのあなたが、
+                    <br/>
+                    <Link to="/">検索エンジン CoffeeCup</Link>を使い業務の技術を調べ直し、
+                    <br/>
+                    技術記事を３つ生成すれば、
+                    <br/>
+                    <span style={section03000yen}>先着順5名様の方にAmazonギフト3000円分</span>を差し上げるキャンペーンです
+                </h5>
                 <h5 style={section1ExplainSP}>
-                    エンジニアの皆様が、<Link to="/">検索エンジン CoffeeCup</Link>を使い記事を３つ自動生成すれば、
-                    <br/>
-                    <span style={section03000yen}>先着順5名の方にAmazonギフト3000円分</span>を差し上げるキャンペーンです
-                    <br/>
                     <small style={section1ExplainSmall}>
                         当選者が５人決まった当日中に３記事生成すれば、先着から外れても1000円分のAmazonギフトを差し上げます
                         <br/>
@@ -202,11 +234,14 @@ const LP2 = () => {
                 <Box sx={section1Title}>
                     <h3 id="participate">参加方法</h3>
                 </Box>
+                <br/>
                 <Box sx={{...section3Main, flexFlow: "column", margin: "30px 20px 20px"}}>
                     <Box sx={{...section3MainContent, width: "100%", marginBottom: "30px"}}>
                         <h5 style={section3MainTitle}>１：検索する</h5>
                         <p style={{lineHeight: "30px", textAlign: "left"}}>
-                            BingとChatGPTでの検索が可能です。時間削減できるので、業務での使用が推奨されます
+                            <span style={section03000yen}>業務中に調べた技術ノウハウ</span>を、この検索エンジンで調べ直しましょう。
+                            <br/>
+                            BingとChatGPTでの検索が可能です。
                             <br/>
                             検索するときは、<span style={section03000yen}>必ず検索する目的を入力するか選択する</span>必要があります
                             <br/>
@@ -256,6 +291,8 @@ const LP2 = () => {
                         Amazonギフト<span style={section03000yen}>3000円</span>獲得！
                     </h3>
                     <p style={{...section0LeftContent}}>
+                        <ModeEditOutlineIcon/> <strong><a href="#why" style={{color:"black"}}>Why?</a></strong>
+                        <br/>
                         <ModeEditOutlineIcon/> <strong><a href="#overview" style={{color:"black"}}>概要{"　"}Overview</a></strong>
                         <br/>
                         <ModeEditOutlineIcon/> <strong><a href="#participate" style={{color:"black"}}>参加方法{"　"}How to participate</a></strong>
@@ -272,14 +309,33 @@ const LP2 = () => {
             <hr style={section0Hr2}/>
             <Box sx={section1}>
                 <Box sx={section1Title}>
+                    <h3 id="why">Why？</h3>
+                </Box>
+                <h5 style={section1WhyExplain}>
+                    あなたの<span style={section03000yen}>履歴書・職務経歴書</span>は技術力をアピールできてますか？
+                    <br/>
+                    文字だけでは、本当の技術力が伝わるか分からなくて心配・・・
+                    <br/>
+                    その心配を解消するために、<span style={section03000yen}>技術力の証拠</span>をアピールしませんか？
+                    <hr style={section1Hr}/>
+                    この検索エンジンは、あなたの検索履歴から記事を自動生成します
+                    <br/>
+                    企業様にアピールしたい技術の記事を<span style={section03000yen}>自動生成</span>して、
+                    <br/>
+                    <span style={section03000yen}>技術的な根拠のある職務経歴書</span>を作りましょう！
+                </h5>
+                <br/><br/>
+                <Box sx={section1Title}>
                     <h3 id="overview">概要{"　"}Overview</h3>
                 </Box>
-                <br/>
+                <h5 style={section1WhyExplain}>
+                    ITエンジニアが、<span style={section03000yen}>検索履歴から記事を自動生成</span>する検索エンジン
+                    <br/>
+                    <Link to="/">CoffeeCup</Link>を使い、IT系技術記事を３つ生成すれば、
+                    <br/>
+                    <span style={section03000yen}>先着順5名様にAmazonギフト3000円分</span>を差し上げるキャンペーンです
+                </h5>
                 <h5 style={section1Explain}>
-                    エンジニアの皆様が、<Link to="/">検索エンジン CoffeeCup</Link>を使い記事を３つ自動生成すれば、
-                    <br/>
-                    <span style={section03000yen}>先着順5名の方にAmazonギフト3000円分</span>を差し上げるキャンペーンです
-                    <br/>
                     <small style={section1ExplainSmall}>
                         当選者が５人決まった当日中に３記事生成すれば、先着から外れても1000円分のAmazonギフトを差し上げます
                         <br/>
@@ -317,9 +373,11 @@ const LP2 = () => {
                 </Box>
                 <Box sx={section3Main}>
                     <Box sx={section3MainContent}>
-                        <h5 style={section3MainTitle}>１：検索する</h5>
+                        <h5 style={section3MainTitle}>１：技術を検索しなおす</h5>
                         <p style={{lineHeight: "30px", textAlign: "left"}}>
-                            BingとChatGPTでの検索が可能です。時間削減できるので、業務での使用が推奨されます
+                            <span style={section03000yen}>業務中に調べた技術ノウハウ</span>をこの検索エンジンで調べ直しましょう。
+                            <br/>
+                            BingとChatGPTでの検索が可能です。
                             <br/>
                             検索するときは、<span style={section03000yen}>必ず検索する目的を入力するか選択する</span>必要があります
                             <br/>
